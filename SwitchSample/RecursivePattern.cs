75c9bc129d309d3c4b9b9f59056f8b22cc83815f
IanChen <ian.chen@taiwantaxi.com.tw>
2020-06-15 13:43:42 +0800

    Recursive Pattern（not null）

--- SwitchSample/RecursivePattern.cs
+++ SwitchSample/RecursivePattern.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace SwitchSample
 {
     public class RecursivePattern
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public RecursivePattern(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public void Index()
         {
@@ -18,6 +27,28 @@ namespace SwitchSample
             var result = Recursive(list);
 
             result.Should().BeEquivalentTo(new { Id = 2, Name = "name2", Score = 24, IsPassed = false });
+        }
+
+        [Fact]
+        public void Index_2()
+        {
+            MyClass3 value = null;
+            if (value is object o)
+            {
+                _testOutputHelper.WriteLine("not null");
+            }
+
+            if (value is MyClass3 v)
+            {
+                _testOutputHelper.WriteLine("not null");
+            }
+
+            if (value is {} x)
+            {
+                _testOutputHelper.WriteLine("not null");
+            }
+
+            _testOutputHelper.WriteLine("is null");
         }
 
         private IEnumerable<MyClass3> Recursive(List<MyClass3> list)
